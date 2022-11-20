8a0629b112a7aec21743b403eb081ca688771d7c
Ravi <raviattrash1@gmail.com>
2022-11-20 10:39:27 +0530

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import Home from "./components/Home";
+//import AddProduct from "./AddBook";
+import Books from './components/Book/Books';
+import About from "./components/About";
+import AddBook from "./components/AddBook";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return <React.Fragment >
+        <
+        header >
+        <
+        Header / >
+        <
+        /header>  <
+    main >
+        <
+        Routes >
+        <
+        Route path = "/"
+    element = { < Home / > }
+    exact
+        /
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <
+        Route path = "/add"
+    element = { < AddBook / > }
+    exact / >
+
+
+        <
+        Route path = "/books"
+    element = { < Books / > }
+    exact / >
+
+
+        <
+        Route path = "/about"
+    element = { < About / > }
+    exact / >
+
+
+
+
+
+
+        <
+        /
+    Routes > <
+        /main >  < /
+    React.Fragment >
 }
 
-export default App;
+export
+default App;
